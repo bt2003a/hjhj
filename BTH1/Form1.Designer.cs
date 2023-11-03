52274c47fccbbb3a74481233329d1740d9a77bf4
bt2003a <149130751+bt2003a@users.noreply.github.com>
2023-11-03 12:01:09 +0700

    update form1

--- BTH1/Form1.Designer.cs
+++ BTH1/Form1.Designer.cs
@@ -29,13 +29,109 @@ namespace BTH1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btCong = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtKetqua = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(265, 53);
+            this.txtA.Multiline = true;
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(368, 53);
+            this.txtA.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(113, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Số a";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btCong
+            // 
+            this.btCong.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCong.Location = new System.Drawing.Point(631, 414);
+            this.btCong.Name = "btCong";
+            this.btCong.Size = new System.Drawing.Size(116, 112);
+            this.btCong.TabIndex = 2;
+            this.btCong.Text = "+";
+            this.btCong.UseVisualStyleBackColor = true;
+            this.btCong.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(113, 196);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 32);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Số b";
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(265, 175);
+            this.txtB.Multiline = true;
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(368, 53);
+            this.txtB.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(113, 292);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 32);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Kết quả";
+            // 
+            // txtKetqua
+            // 
+            this.txtKetqua.Location = new System.Drawing.Point(265, 271);
+            this.txtKetqua.Multiline = true;
+            this.txtKetqua.Name = "txtKetqua";
+            this.txtKetqua.Size = new System.Drawing.Size(368, 53);
+            this.txtKetqua.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 622);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtKetqua);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.btCong);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtA);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btCong;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtKetqua;
     }
 }
 
